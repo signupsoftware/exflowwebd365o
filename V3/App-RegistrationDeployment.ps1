dfe985584016a478aba4ea93321f821ab625886e
thomasonsignup <thomas.backman@signupsoftware.com>
2020-02-26 18:27:42 +0100

    addresourcegroup

--- V3/App-RegistrationDeployment.ps1
+++ V3/App-RegistrationDeployment.ps1
@@ -238,7 +238,7 @@ $TemplateParameters = @{
     #KeyValueStorageConnection     = "DefaultEndpointsProtocol=https;AccountName=$($StorageName);AccountKey=$($Keys[0].Value);"
 }
 "$($RepoURL)WebSite.json"
-New-AzResourceGroupDeployment -TemplateParameterObject $TemplateParameters -TemplateUri "$($RepoURL)WebSite.json" -Name "abc" -Verbose
+New-AzResourceGroupDeployment -TemplateParameterObject $TemplateParameters -TemplateUri "$($RepoURL)WebSite.json" -Name "abc" -ResourceGroupName $ResourceGroup -Verbose
 
 $Measure.Stop()
 
