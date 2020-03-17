0beaa72595cdd5010b2fe6661c011cdad7eedb48
thomasonsignup <thomas.backman@signupsoftware.com>
2020-03-17 12:05:45 +0100

    manual

--- V3/App-RegistrationDeployment.ps1
+++ V3/App-RegistrationDeployment.ps1
@@ -547,6 +547,8 @@ If(($AzAadApp = az ad app list --display-name $ResourceGroup <#Get-AzADApplicati
     $psadCredential.Password
     ("https://$($DeploymentName).$($ConfigurationData.AzureRmDomain)/inbox.aspx")
     $AzAadApp = az ad app update --id $AzAadApp.appId --display-name $ResourceGroup --identifier-uris ("https://$($DeploymentName).$($ConfigurationData.AzureRmDomain)/inbox.aspx") --password $psadCredential.Password --reply-urls ("https://$($DeploymentName).$($ConfigurationData.AzureRmDomain)/inbox.aspx") --required-resource-accesses $requiredresourceaccesses --end-date ($(get-date).AddYears(20))
+    az ad app update --id ffefb40c-f8c1-417d-bd12-eabf75819f79 --display-name exflowththingy08e33b592b --identifier-uris "https://exflowththingy08e33b592b.azurewebsites.net/inbox.aspx" --password "abc" --reply-urls "https://exflowththingy08e33b592b.azurewebsites.net/inbox.aspx" --end-date ($(get-date).AddYears(30)) --required-resource-accesses $requiredresourceaccesses --verbose
+
     if (!$AzAadApp) { 
         Write-Warning "Unable to create or Update Az App, verify that account logged in has correct permissions"
         Write-Output "Logged in to tenant: $($AzCliLogin[0].tenantId) as user: $($AzCliLogin[0].user.name)"
