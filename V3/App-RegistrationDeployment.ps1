14d51d119756e95729afad4f1ad6a20cfb8c8e9b
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2020-11-10 16:39:35 +0100

    Update App-RegistrationDeployment.ps1

--- V3/App-RegistrationDeployment.ps1
+++ V3/App-RegistrationDeployment.ps1
@@ -522,7 +522,7 @@ If ($WebApp) {
     write-output $replyUrls 
 }
 $requiredresourceaccesses = '[{"resourceAppId": "00000002-0000-0000-c000-000000000000","resourceAccess": [{"id": "311a71cc-e848-46a1-bdf8-97ff7156d8e6","type": "Scope"}]},{"resourceAppId": "00000015-0000-0000-c000-000000000000","resourceAccess": [{"id": "6397893c-2260-496b-a41d-2f1f15b16ff3","type": "Scope"},{"id": "a849e696-ce45-464a-81de-e5c5b45519c1","type": "Scope"},{"id": "ad8b4a5c-eecd-431a-a46f-33c060012ae1","type": "Scope"}]}]' | convertto-json
-If(($AzAadApp = az ad app list --display-name $DeploymentName) -eq "[]") {
+If(($AzAadApp = az ad app list --filter "displayname eq `'$DeploymentName`'") -eq "[]") {
     Write-Output "Creating new Azure AD Application"
     try {
     $ErrorActionPreference = "Continue"
@@ -551,7 +551,7 @@ If(($AzAadApp = az ad app list --display-name $DeploymentName) -eq "[]") {
         Write-Output $error
         break
     } else {
-        $AzAadApp = az ad app list --app-id $AzAadApp.appId
+        $AzAadApp = az ad app list --filter "displayname eq `'$DeploymentName`'"
     }
 }
 If ($AzAadApp) {
