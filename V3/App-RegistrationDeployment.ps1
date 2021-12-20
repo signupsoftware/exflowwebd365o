f652cfa73ed919a21ce4791dda907f72e31a982e
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2021-12-20 13:39:23 +0100

    Update App-RegistrationDeployment.ps1
    updates

--- V3/App-RegistrationDeployment.ps1
+++ V3/App-RegistrationDeployment.ps1
@@ -536,7 +536,7 @@ If(($AzAadApp = az ad app list --filter "displayname eq `'$DeploymentName`'") -e
     Write-Output "Creating new Azure AD Application"
     try {
     $ErrorActionPreference = "Continue"
-    $AzAadApp = az ad app create --display-name $DeploymentName --identifier-uris ("https://$($DeploymentName).$CurrentTenantDomain/inbox.aspx") <#--password $psadCredential.Password#> --reply-urls $replyUrls --required-resource-accesses $requiredresourceaccesses --end-date ($psadCredential.EndDate)
+    $AzAadApp = az ad app create --display-name $DeploymentName --identifier-uris ("https://$($DeploymentName).$CurrentTenantDomain/inbox.aspx") <#--password $psadCredential.Password#> --reply-urls $replyUrls --required-resource-accesses $requiredresourceaccesses <#-end-date ($psadCredential.EndDate)#>
     $psadCredential = ad app credential reset --id ($AzAadApp | ConvertFrom-Json).appId --end-date $endDate | ConvertFrom-Json
     if (!$AzAadApp) { 
         Write-Warning "Unable to create or Update Az App, verify that account logged in has correct permissions"
@@ -553,7 +553,7 @@ If(($AzAadApp = az ad app list --filter "displayname eq `'$DeploymentName`'") -e
     $AzAadApp = $AzAadApp | ConvertFrom-Json
     $error.clear()
     Write-Output "az ad app update --id $($AzAadApp.appId) --display-name $($DeploymentName) --identifier-uris $(("https://$($DeploymentName).$CurrentTenantDomain/inbox.aspx")) --password $($psadCredential.Password) --reply-urls $($replyUrls) --required-resource-accesses $($requiredresourceaccesses) --end-date $($psadCredential.EndDate)"
-    #az ad app update --id $AzAadApp.appId --display-name $DeploymentName --identifier-uris ("https://$($DeploymentName).$CurrentTenantDomain/inbox.aspx") --password $psadCredential.Password --reply-urls $replyUrls --required-resource-accesses $requiredresourceaccesses --end-date ($psadCredential.EndDate)
+    #az ad app update --id $AzAadApp.appId --display-name $DeploymentName --identifier-uris ("https://$($DeploymentName).$CurrentTenantDomain/inbox.aspx") <#--password $psadCredential.Password#> --reply-urls $replyUrls --required-resource-accesses $requiredresourceaccesses <#--end-date ($psadCredential.EndDate)#>
     $psadCredential = az ad app credential reset --id $AzAadApp.appId --end-date $endDate | ConvertFrom-Json
     if ($error) { 
         Write-Warning "Unable to create or Update Az App, verify that account logged in has correct permissions"
